db627a099467255a3d01991efaf5872f960a99c1
student-19ofit <blossomlk@gmail.com>
2024-10-27 21:24:42 +0500

    first commit

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, } from '../env'
 
 export const client = createClient({
-  projectId,
+  projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
