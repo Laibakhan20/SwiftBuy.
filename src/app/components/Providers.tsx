79863d80c0249eb6089ebd0821e1512af8e9a2ee
student-19ofit <blossomlk@gmail.com>
2024-10-28 11:48:33 +0500

    update

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -10,8 +10,8 @@ export default function Provider({ children }: { children: ReactNode }) {
         mode="payment"
        cartMode="client-only"
        stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-       successUrl="http://localhost:3000/stripe/success"
-       cancelUrl="http://localhost:3000/stripe/cancel"
+       successUrl="https://swift-buy-five.vercel.app/stripe/success"
+       cancelUrl="https://swift-buy-five.vercel.app/stripe/cancel"
        currency="USD"
        billingAddressCollection={false}
        shouldPersist={true}
